38cdde6c3737f75d2be054dfbba8452ac8a797b5
Imran Muhammad <imranwebdevelop@gmail.com>
2024-09-07 08:47:54 +0300

    building Footer component.

---
+++ client/Netflix/src/components/Footer.jsx
@@ -0,0 +1,82 @@
+function Footer() {
+  return (
+    <>
+      <footer className="footer container">
+        <section className="footer__ask">
+          <div className="ps-4">
+            <a href="#">Question? Contact us.</a>
+          </div>
+        </section>
+        <section className="footer__content grid grid__1x4">
+          <ul className="footer__list">
+            <li className="footer__item">
+              <a href="">FAQ</a>
+            </li>
+            <li className="footer__item">
+              <a href="">Investor Relations</a>
+            </li>
+            <li className="footer__item">
+              <a href="">Buy Gift Cards </a>
+            </li>
+            <li className="footer__item">
+              <a href="">Cookie Preferences</a>
+            </li>
+            <li className="footer__item">
+              <a href="">Speed Test</a>
+            </li>
+          </ul>
+          <ul>
+            <li className="footer__item">
+              <a href="">Help Center</a>
+            </li>
+            <li className="footer__item">
+              <a href="">Jobs</a>
+            </li>
+            <li className="footer__item">
+              <a href="">Ways to Watch</a>
+            </li>
+            <li className="footer__item">
+              <a href="">Corporate Information </a>
+            </li>
+            <li className="footer__item">
+              <a href="">Legal Notices</a>
+            </li>
+          </ul>
+          <ul>
+            <li className="footer__item">
+              <a href="">Account</a>
+            </li>
+            <li className="footer__item">
+              <a href="">Netflix Shop</a>
+            </li>
+            <li className="footer__item">
+              <a href="">Terms of Use </a>
+            </li>
+            <li className="footer__item">
+              <a href="">Contact Us </a>
+            </li>
+            <li className="footer__item">
+              <a href="">Only on Netflix</a>
+            </li>
+          </ul>
+          <ul>
+            <li className="footer__item">
+              <a href="">Media Center</a>
+            </li>
+            <li className="footer__item">
+              <a href="">Redeem Gift Cards</a>
+            </li>
+            <li className="footer__item">
+              <a href="">Privacy</a>
+            </li>
+            <li className="footer__item">
+              <a href="">Act on Specified Commercial Transactions</a>
+            </li>
+          </ul>
+        </section>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
--- client/Netflix/src/pages/Home.jsx
+++ client/Netflix/src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import Header from "../components/Header";
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 function Home() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
